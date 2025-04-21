32b5b269143987c14b73b37f8aa65a540a64e34c
zaidbinnaveed <142867727+zaidbinnaveed@users.noreply.github.com>
2025-04-21 18:35:14 +0500

    Create Task 01.py

---
+++ Lab 08/Task 01.py
@@ -0,0 +1,9 @@
+p_red = 26 / 52
+p_heart_given_red = 13 / 26
+p_diamond_given_face = 3 / 12
+p_spade_or_queen_given_face = (3 + 1) / 12
+
+print(p_red)
+print(p_heart_given_red)
+print(p_diamond_given_face)
+print(p_spade_or_queen_given_face)
